81aad2705817e54c299ccb54b253196308e1f77b
Checkson <952052437@qq.com>
2019-03-08 15:03:56 +0800

    -

---
+++ codes/DLink.js
@@ -0,0 +1,6 @@
+// 双链表构造函数
+function DLink () {
+    this.head = new DNode('head');
+}
+
+// 
---
+++ codes/DNode.js
@@ -0,0 +1,6 @@
+// 双向链表节点类
+function DNode (el) {
+    this.el = el;
+    this.prev = null;
+    this.next = null;
+}
